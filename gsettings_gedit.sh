e7fe3817facad3311aeb4d9c89e317444adc6aef
Hector Castro <hector.hecaxmmx@gmail.com>
2017-05-12 11:46:54 -0600

    Fixing mistakes in gsettings key values

--- gsettings_gedit.sh
+++ gsettings_gedit.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Config gedit
-gsettings set org.gnome.gedit.plugins active-plugins ['charmap', 'docinfo', 'changecase', 'drawspaces', 'spell', 'snippets', 'time', 'externaltools', 'modelines', 'filebrowser']
-gsettings set org.gnome.gedit.plugins.drawspaces draw-spaces ['space', 'tab', 'leading', 'text', 'trailing']
-gsettings set org.gnome.gedit.plugins.drawspaces enable true
+gsettings set org.gnome.gedit.plugins active-plugins "['spell', 'docinfo', 'drawspaces', 'snippets', 'time', 'externaltools', 'modelines', 'filebrowser']"
+gsettings set org.gnome.gedit.plugins.drawspaces show-white-space true
+gsettings set org.gnome.gedit.plugins.drawspaces draw-spaces "['space', 'tab', 'leading', 'text', 'trailing']"
+gsettings set org.gnome.gedit.plugins.time selected-format '%c'
+gsettings set org.gnome.gedit.plugins.time prompt-type 'prompt-custom-format'
+gsettings set org.gnome.gedit.plugins.time custom-format '%Y%m%d%H%M%S.0'
 gsettings set org.gnome.gedit.preferences.editor bracket-matching true
 gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
 gsettings set org.gnome.gedit.preferences.editor display-right-margin false
@@ -10,4 +13,4 @@ gsettings set org.gnome.gedit.preferences.editor ensure-trailing-newline true
 gsettings set org.gnome.gedit.preferences.editor highlight-current-line true
 gsettings set org.gnome.gedit.preferences.editor syntax-highlighting true
 gsettings set org.gnome.gedit.preferences.ui side-panel-visible false
-gsettings set org.gnome.gedit.preferences.encodings auto-detected ['UTF-8', 'CURRENT', 'ISO-8859-15', 'UTF-16']
+gsettings set org.gnome.gedit.preferences.encodings auto-detected "['UTF-8', 'CURRENT', 'ISO-8859-15', 'UTF-16']"
